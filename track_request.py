40f6f06b367b55758df0115accd322d33d55b845
Tim Petro <tim@lyteworx.com>
2019-02-25 21:52:29 -0500

    Initial commit

---
+++ track_request.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import random
+
+desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+
+def random_headers():
+    return {'User-Agent': random.choice(desktop_agents),'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' }
+
+url = "https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=7200&gliders=1&stats=1"
+
+responce = requests.get( url, headers=random_headers() )
+
+data = json.loads(responce.text)
+
+#print(type(data));
+# print(data)
+
+# // Pull Key's from Dict and create a new List.
+flight_list = list(data.keys())
+
+# print (type(data.get('1f89480e')))
+
+# Get a random flight from the flight list
+# and pull the flight information from data.
+
+random_flight = random.choice(flight_list)
+
+
+
+print("Random Flight: ", random_flight)
+print(data.get(random_flight))
+
+# Description of information from each flight
+#0 - Mode S
+#1 - Latitude
+#2 - Longitude
+#3 - Heading
+#4 - Altitude
+#5 - Speed/Knots
+#6
+#7
+#8 - Type Code
+#9 - Registration
+#10
+#11 - Source Airport Code
+#12 - Destination Airport Code
+#13 - Flight # Airline/Operator
+#14 - Empty
+#15 - Empty
+#16 -
+#17 - Empty
+#18 - Country Code
